aec8bb381ed67ee590add25c09ddb296f5722f64
SANJAY GAUTAM <SANJAY GAUTAM@sanjaykumarsingh9419>
2021-09-17 18:18:51 +0530

    WH-12-new update

--- src/springhandling/Test.java
+++ src/springhandling/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hellos..");
+      System.out.println("i am fine");
+      System.out.println("how are you!");
 	}
 
 }
